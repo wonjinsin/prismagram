b9c4f2129524b4661192a3f4d31cf916550700f4
wonjinsin <wonjinsin@wonjinui-MacBookPro.local>
2019-12-01 19:46:09 +0900

    add someinfo Becauseof  prismagram-frontend until FrontEnd LogOut

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,6 +2,10 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Post: {
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
+    likes: ({ id }) => prisma.post({ id }).likes(),
     isLiked: (parent, _, { request }) => {
       const { user } = request;
       const { id } = parent;
@@ -21,8 +25,18 @@ export default {
       });
     },
     likeCount: parent =>
-      prisma.likesConnection({
-        where: { post: { id: parent.id } }
-      }).aggregate().count()
+      prisma
+        .likesConnection({
+          where: { post: { id: parent.id } }
+        })
+        .aggregate()
+        .count(),
+    commentCount: parent =>
+      prisma
+        .commentsConnection({
+          where: { post: { id: parent.id } }
+        })
+        .aggregate()
+        .count()
   }
 };
